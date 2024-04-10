5fdd2f8d9145dbb220e1394e217a86712826eb9e
ezzkimm <dmswjd1515@gmail.com>
2024-04-10 21:51:02 +0900

    Initial Commit

---
+++ nano_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nano_1
+//
+//  Created by 김은정 on 4/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nano_1/nano_1App.swift
@@ -0,0 +1,17 @@
+//
+//  nano_1App.swift
+//  nano_1
+//
+//  Created by 김은정 on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct nano_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
