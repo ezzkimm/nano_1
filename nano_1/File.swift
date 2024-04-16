5beb0ea5976a71125f5c33d216084a2666734b17
ezzkimm <dmswjd1515@gmail.com>
2024-04-16 12:10:31 +0900

    240416 1210 navigationlink update

---
+++ nano_1/File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  nano_1
+//
+//  Created by 김은정 on 4/16/24.
+//
+
+import Foundation
---
+++ nano_1/InputData.swift
@@ -0,0 +1,8 @@
+//
+//  InputData.swift
+//  nano_1
+//
+//  Created by 김은정 on 4/15/24.
+//
+
+import Foundation
