ee1e4488b938fc5da5c7e757481cf2654bc5d27b
ezzkimm <dmswjd1515@gmail.com>
2024-04-18 22:19:14 +0900

    240418 2219 swiftdata

---
+++ nano_1/EmojiDraw.swift
@@ -0,0 +1,18 @@
+//
+//  EmojiDraw.swift
+//  nano_1
+//
+//  Created by 김은정 on 4/18/24.
+//
+
+import SwiftUI
+
+struct EmojiDraw: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmojiDraw()
+}
